4fcfbb13c57894b2646e48d41c8dd69d8e16e35c
Mickey <mickey.welensky@gmail.com>
2017-12-28 18:58:29 +0100

    Ticket Entity Integration
    Ticket model entity integration with relation Many to One to Booking Entity

---
+++ src/Surikat/BookingBundle/Entity/Ticket.php
@@ -0,0 +1,313 @@
+<?php
+
+namespace Surikat\BookingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Ticket
+ *
+ * @ORM\Table(name="ticket")
+ * @ORM\Entity(repositoryClass="Surikat\BookingBundle\Repository\TicketRepository")
+ */
+class Ticket
+{
+
+    /**
+    * @ORM\ManyToOne(targetEntity="Surikat\BookingBundle\Entity\Booking")
+    * @ORM\JoinColumn(nullable=false)
+    */
+    private $booking;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="surname", type="string", length=255)
+     */
+    private $surname;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="birthdate", type="datetime")
+     */
+    private $birthdate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, nullable=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="code", type="string", length=255)
+     */
+    private $code;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="price", type="integer")
+     */
+    private $price;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="info", type="string", length=255, nullable=true)
+     */
+    private $info;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="bookedDate", type="datetime")
+     */
+    private $bookedDate;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Ticket
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set surname
+     *
+     * @param string $surname
+     *
+     * @return Ticket
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+
+        return $this;
+    }
+
+    /**
+     * Get surname
+     *
+     * @return string
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * Set birthdate
+     *
+     * @param \DateTime $birthdate
+     *
+     * @return Ticket
+     */
+    public function setBirthdate($birthdate)
+    {
+        $this->birthdate = $birthdate;
+
+        return $this;
+    }
+
+    /**
+     * Get birthdate
+     *
+     * @return \DateTime
+     */
+    public function getBirthdate()
+    {
+        return $this->birthdate;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Ticket
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set price
+     *
+     * @param integer $price
+     *
+     * @return Ticket
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return int
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set info
+     *
+     * @param string $info
+     *
+     * @return Ticket
+     */
+    public function setInfo($info)
+    {
+        $this->info = $info;
+
+        return $this;
+    }
+
+    /**
+     * Get info
+     *
+     * @return string
+     */
+    public function getInfo()
+    {
+        return $this->info;
+    }
+
+    /**
+     * Set bookedDate
+     *
+     * @param \DateTime $bookedDate
+     *
+     * @return Ticket
+     */
+    public function setBookedDate($bookedDate)
+    {
+        $this->bookedDate = $bookedDate;
+
+        return $this;
+    }
+
+    /**
+     * Get bookedDate
+     *
+     * @return \DateTime
+     */
+    public function getBookedDate()
+    {
+        return $this->bookedDate;
+    }
+
+    /**
+     * Set code
+     *
+     * @param string $code
+     *
+     * @return Ticket
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    /**
+     * Get code
+     *
+     * @return string
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * Set booking
+     *
+     * @param \Surikat\BookingBundle\Entity\Booking $booking
+     *
+     * @return Ticket
+     */
+    public function setBooking(\Surikat\BookingBundle\Entity\Booking $booking)
+    {
+        $this->booking = $booking;
+
+        return $this;
+    }
+
+    /**
+     * Get booking
+     *
+     * @return \Surikat\BookingBundle\Entity\Booking
+     */
+    public function getBooking()
+    {
+        return $this->booking;
+    }
+}
