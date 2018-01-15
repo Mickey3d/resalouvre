3afe67f932462ea87831b93316ea93505d34ae17
Mickey <mickey.welensky@gmail.com>
2018-01-15 14:40:07 +0100

    Step-9-Bis Datepicker integration Issues  fixed
    - Issues origin was a line of code  duplicated and creating a js execution conflict

--- web/assets/js/script.js
+++ web/assets/js/script.js
@@ -26,7 +26,6 @@ $(document).ready(function() {
   }, 7000);
 
 
-
   // On récupère la balise <div> en question qui contient l'attribut « data-prototype » qui nous intéresse.
   var $container = $('div#surikat_bookingbundle_booking_tickets');
 
@@ -36,6 +35,7 @@ $(document).ready(function() {
   // On ajoute un nouveau champ à chaque clic sur le lien d'ajout.
   $('#add_ticket').click(function(e) {
     addTicket($container);
+
     e.preventDefault(); // évite qu'un # apparaisse dans l'URL
     return false;
   });
@@ -47,8 +47,6 @@ $(document).ready(function() {
     // On ajoute un lien de suppression pour chacun des tickets
     $container.children('div').each(function() {
       addDeleteLink($(this));
-      
- //     addDatePicker();
     });
   }
 
@@ -57,11 +55,9 @@ $(document).ready(function() {
     // Dans le contenu de l'attribut « data-prototype », on remplace :
     // - le texte "__name__label__" qu'il contient par le label du champ
     // - le texte "__name__" qu'il contient par le numéro du champ
-    var template = $container.attr('data-prototype')
-    .replace(/__name__label__/g, 'Ticket n°' + (
+    var template = $container.attr('data-prototype').replace(/__name__label__/g, 'Ticket n°' + (
       index + 1
-    ))
-    .replace(/__name__/g, index);
+    )).replace(/__name__/g, index);
 
     // On crée un objet jquery qui contient ce template
     var $prototype = $(template);
@@ -71,21 +67,20 @@ $(document).ready(function() {
 
     // On ajoute le prototype modifié à la fin de la balise <div>
     $container.append($prototype);
-    //  console.log($('div#surikat_bookingbundle_booking_tickets > div').length)
-    //    addDatePicker();
-    /*
+
+    // configure the bootstrap datepicker
     $('.js-datepicker').datepicker({
-              startView: 2,
-              clearBtn: true,
-              language: "fr",
-              format: "dd/mm/yyyy",
-              startDate: '01/01/1900',
-              endDate: '-1d',            
-              startView: 3,
-              defaultViewDate: { year: 1980, month: 06, day: 15 },
-              autoclose: true,
-      });
-      */
+      startView: 2,
+      format: 'dd-mm-yyyy',
+      clearBtn: true,
+      language: "fr",
+      startDate: '01/01/1900',
+      endDate: '-1d',
+      startView: 3,
+      defaultViewDate: { year: 1980, month: 06, day: 15 },
+      autoclose: true,
+      })
+
     // Enfin, on incrémente le compteur pour que le prochain ajout se fasse avec un autre numéro
     index++;
   }
@@ -94,7 +89,7 @@ $(document).ready(function() {
   function addDeleteLink($prototype) {
     // Création du lien
     var $deleteLink = $('<div><a href="#" title="Supprimer de la Commande" class="pull-right btn btn-delete"><i class="fa fa-trash-o" aria-hidden="true"></i><strong> </strong></a></br><hr height="none" size="3" width="50%" color="blue"></div>');
-    
+
     if (($('div#surikat_bookingbundle_booking_tickets > div').length) >= 1) {
     // Ajout du lien
     $prototype.append($deleteLink);
@@ -103,29 +98,26 @@ $(document).ready(function() {
     $deleteLink.click(function(e) {
       if (($('div#surikat_bookingbundle_booking_tickets > div').length) > 1) {
                 $prototype.remove();          
-      }
-    // console.log($('div#surikat_bookingbundle_booking_tickets > div').length)
+    }
       e.preventDefault(); // évite qu'un # apparaisse dans l'URL
       return false;
     });
   }
 
-
-});
 /*
   // configure the bootstrap datepicker
- $(document).on('focus', ".js-datepicker", function() {
-  $(this).datepicker({
+  $('.js-datepicker').datepicker({
     startView: 2,
-          clearBtn: true,
-          language: "fr",
-          format: "dd/mm/yyyy",
-          startDate: '01/01/1900',
-          endDate: '-1d',            startView: 3,
-          defaultViewDate: { year: 1980, month: 06, day: 15 },
-          autoclose: true,
-  });
-});
+    format: 'dd-mm-yyyy',
+    clearBtn: true,
+    language: "fr",
+    startDate: '01/01/1900',
+    endDate: '-1d',
+    startView: 3,
+    defaultViewDate: { year: 1980, month: 06, day: 15 },
+    autoclose: true,
+  })
 */
+});
 
 
